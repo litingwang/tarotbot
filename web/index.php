1f6c2a471bc4a4adf8c1df5b6e3c8b3713d61c54
liting.wang <liting.wang@tgc-taiwan.com.tw>
2018-11-28 17:54:02 +0800

    test

--- web/index.php
+++ web/index.php
@@ -68,7 +68,7 @@ class user {
         $this->curl = new Curl();
         $url_api = "https://api.line.me/v2/bot/profile/".$this->userID;
 
-        $data_url, $data_type,$data_userpwd, $authorization
+        //$data_url, $data_type,$data_userpwd, $authorization
         $output = $this->curl->curl_get($url_api,'auth',false,$channelAccessToken);
         $arr_result = json_decode($output,true);
         return $arr_result['displayName'];
